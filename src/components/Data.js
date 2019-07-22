e1d5d311773555a678d419b3330a5c9bdd69cdd0
Dou Jones <misterchiefexecutiveofficer@yahoo.com>
2019-07-22 00:45:24 -0400

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,101 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: 'Fulton County Library',
+    routeName: 'books',
+    items: [
+      
+      {
+        id: 1,
+        name: 'MLK Biography',
+        imageUrl: 'https://images.pexels.com/photos/34592/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 325
+      },
+      {
+        id: 2,
+        name: 'Malcolm X 1965',
+        imageUrl: 'https://images.pexels.com/photos/45717/pexels-photo-45717.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 20
+      },
+      {
+        id: 3,
+        name: 'American History',
+        imageUrl: 'https://images.pexels.com/photos/415078/pexels-photo-415078.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 25
+      },
+      {
+        id: 4,
+        name: 'Oregon Trail',
+        imageUrl: 'https://images.pexels.com/photos/1112048/pexels-photo-1112048.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 25
+      },
+      {
+        id: 5,
+        name: 'Sunshine State',
+        imageUrl: 'https://images.pexels.com/photos/768125/pexels-photo-768125.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 40
+      },
+      {
+        id: 6,
+        name: 'The Rise of Alabama Crimson',
+        imageUrl: 'https://images.pexels.com/photos/1370295/pexels-photo-1370295.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 25
+      },
+      {
+        id: 7,
+        name: 'The Goat',
+        imageUrl: 'https://images.pexels.com/photos/159621/open-book-library-education-read-159621.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 25
+      },
+      {
+        id: 8,
+        name: 'Netflix vs Hulu',
+        imageUrl: 'https://images.pexels.com/photos/264635/pexels-photo-264635.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 25
+      },
+      {
+        id: 9,
+        name: 'Serial Killers',
+        imageUrl: 'https://images.pexels.com/photos/1148399/pexels-photo-1148399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 25
+      },
+      {
+        id: 10,
+        name: 'Crossover by Jordan',
+        imageUrl: 'https://images.pexels.com/photos/1148399/pexels-photo-1148399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 25
+      },
+      {
+        id: 11,
+        name: 'Bernie Sanders the Senator',
+        imageUrl: 'https://images.pexels.com/photos/1166657/pexels-photo-1166657.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 25
+      },
+      {
+        id: 12,
+        name: 'George Washington Carver the Farmer',
+        imageUrl: 'https://images.pexels.com/photos/51342/books-education-school-literature-51342.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 25
+      },
+      {
+        id: 13,
+        name: 'Ghetto Slums',
+        imageUrl: 'https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 25
+      },
+      {
+        id: 14,
+        name: 'NBA vs NFL',
+        imageUrl: 'https://images.pexels.com/photos/590493/pexels-photo-590493.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 25
+      },
+      {
+        id: 15,
+        name: 'Globalization',
+        imageUrl: 'https://images.pexels.com/photos/159866/books-book-pages-read-literature-159866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 25
+      } 
+    
+    ]
+  }
+]
